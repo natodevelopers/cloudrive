2e9d8f9a772a3452c28809a342aa79a8fee67f8d
Nato Developers <developers.nato@gmail.com>
2026-01-02 21:16:34 +0700

    Remove unused font and metadata exports

--- app/layout.js
+++ app/layout.js
@@ -11,26 +11,16 @@ export const helveticaNeue = localFont({
   ],
 });
 
-export const sohneBreit = localFont({
-  src: [
-    {
-      path: "../public/fonts/SohneBreit-Halbfett.woff2",
-      weight: "700",
-    },
-  ],
-});
-
-export const metadata = {
-  title: "Cloudrive",
-  other: {
-    "google-site-verification":
-      "pjDL48b1IMVte3ajtoeTdKOHwJspD1mho6KFXM6pKfk",
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Cloudrive</title>
+        <meta
+          name="google-site-verification"
+          content="pjDL48b1IMVte3ajtoeTdKOHwJspD1mho6KFXM6pKfk"
+        />
+      </head>
       <body className={helveticaNeue.className}>
         <Provider>{children}</Provider>
       </body>
