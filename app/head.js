9d838133d65331be2d0d70ebe636fe4ea4144d71
Nato Developers <developers.nato@gmail.com>
2026-01-03 18:49:31 +0700

    Add Head component with title and meta tags

---
+++ app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Cloudrive</title>
+      <meta
+        name="google-site-verification"
+        content="pjDL48b1IMVte3ajtoeTdKOHwJspD1mho6KFXM6pKfk"
+      />
+    </>
+  );
+}
