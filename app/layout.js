a494c1774301f22e444360474a725bca5e70e256
Nato Developers <developers.nato@gmail.com>
2026-01-03 18:42:43 +0700

    Replace HelveticaNeue with SohneBreit font

--- app/layout.js
+++ app/layout.js
@@ -2,26 +2,23 @@ import "./globals.css";
 import localFont from "next/font/local";
 import Provider from "../components/Provider";
 
-const helveticaNeue = localFont({
-  src: [
-    {
-      path: "../public/fonts/HelveticaNeue-Light.woff2",
-      weight: "300",
-    },
-  ],
+const sohneBreit = localFont({
+  src: "../public/fonts/HelveticaNeue-Light.woff2",
+  weight: "300",
 });
+
+export const metadata = {
+  title: "Cloudrive",
+  other: {
+    "google-site-verification":
+      "pjDL48b1IMVte3ajtoeTdKOHwJspD1mho6KFXM6pKfk",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>Cloudrive</title>
-        <meta
-          name="google-site-verification"
-          content="pjDL48b1IMVte3ajtoeTdKOHwJspD1mho6KFXM6pKfk"
-        />
-      </head>
-      <body className={helveticaNeue.className}>
+      <body className={sohneBreit.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
