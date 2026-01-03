64a5121e6b880eab897e6c1d78b4f61a2739d4ec
Nato Developers <developers.nato@gmail.com>
2026-01-03 18:43:03 +0700

    Refactor Google sign-in button and animations
    Updated the Google sign-in section with improved animations and styling.

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@
 import { motion } from "framer-motion";
 import { useSession, signIn } from "next-auth/react";
 import Image from "next/image";
-import googleIcon from "@/public/google.svg";
 import { redirect } from "next/navigation";
 import styles from "./page.module.css";
+import googleIcon from "@/public/google.svg";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -17,7 +17,7 @@ export default function Home() {
       <motion.p
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: [0, 0, 0, 0.8] }}
+        transition={{ duration: 1 }}
         className={styles.welcome}
       >
         Hi! This is
@@ -26,33 +26,28 @@ export default function Home() {
       <motion.h1
         initial={{ opacity: 0, y: 35 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: [0, 0, 0, 0.8], delay: 0.5 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
         className={styles.h1}
       >
         CLOUDRIVE
       </motion.h1>
 
-      <div className={styles.contentWrapper}>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.5, ease: "easeOut", delay: 1 }}
-          className={`border ${styles.content}`}
-        >
-          <p>
-            Try it now by sign in with Google below, and you&apos;ll see the power
-            of this ;)
-          </p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.8 }}
+        className={styles.card}
+      >
+        <p>Try it now by signing in with Google 👇</p>
 
-          <button
-            onClick={() => signIn("google", { callbackUrl: "/upload" })}
-            className={`button ${styles.buttonWithIcon}`}
-          >
-            <Image src={googleIcon} width={22} height={22} alt="Google" />
-            Login with Google now!
-          </button>
-        </motion.div>
-      </div>
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/upload" })}
+          className={styles.button}
+        >
+          <Image src={googleIcon} width={22} height={22} alt="Google" />
+          Login with Google
+        </button>
+      </motion.div>
     </main>
   );
 }
