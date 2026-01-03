d2b4319449728108a310950fa1694325b0d426cf
Nato Developers <developers.nato@gmail.com>
2026-01-03 18:50:07 +0700

    Remove metadata export from layout.js
    Removed metadata export for title and verification.

--- app/layout.js
+++ app/layout.js
@@ -7,14 +7,6 @@ const helveticaNeue = localFont({
   weight: "300",
 });
 
-export const metadata = {
-  title: "Cloudrive",
-  other: {
-    "google-site-verification":
-      "pjDL48b1IMVte3ajtoeTdKOHwJspD1mho6KFXM6pKfk",
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
